b7605cf59114d6d47ae21d911516c3c180e2c24e
Naveenaece2000 <naveenaece2000@gmail.com>
2026-02-13 09:21:28 +0000

    Edit outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,14 @@
 output "public_ip" {
-  value = aws_instance.devops_user.public_ip
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.devops_user.public_ip
 }
 
 output "instance_id" {
-  value = aws_instance.devops_user.id
+  description = "ID of the EC2 instance"
+  value       = aws_instance.devops_user.id
 }
 
 output "directus_url" {
-  value = "http://${aws_instance.devops_user.public_ip}"
+  description = "Directus application URL"
+  value       = "http://${aws_instance.devops_user.public_ip}"
 }
